77188da0613ab3d0a50ee56aa0c7ed3ec71ae0a6
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-08 21:10:31 +0800

    :heavy_plus_sign:  new customer

---
+++ SFC.Canteen/Views/NewCustomer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace SFC.Canteen.Views
+{
+    /// <summary>
+    /// Interaction logic for NewCustomer.xaml
+    /// </summary>
+    public partial class NewCustomer : Window
+    {
+        public NewCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void AcceptClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
