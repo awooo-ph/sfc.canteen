2989e60d5dcea892f1fa8cbcf58972f9db1e3ab9
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-08 21:10:18 +0800

    :heavy_plus_sign:  new product

---
+++ SFC.Canteen/Views/NewProduct.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace SFC.Canteen.Views
+{
+    /// <summary>
+    /// Interaction logic for NewProduct.xaml
+    /// </summary>
+    public partial class NewProduct : Window
+    {
+        public NewProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void AcceptClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
