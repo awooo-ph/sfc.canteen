57b959bb426685221fff2b26de068f9af57d8244
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-08 21:05:04 +0800

    initialize solution

---
+++ SFC.Canteen/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace SFC.Canteen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            awooo.IsRunning = true;
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            awooo.IsRunning = false;
+            base.OnExit(e);
+        }
+    }
+}
