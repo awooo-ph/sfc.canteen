298d5d1dc07b69ea6c54c374e6e9cc52ffcc35b1
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-08 21:07:05 +0800

    :heavy_plus_sign:

---
+++ SFC.Canteen/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace SFC.Canteen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            awooo.Context = SynchronizationContext.Current;
+        }
+    }
+}
