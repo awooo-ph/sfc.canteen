cee730ace244940172d9ba72b22d205605db7d1d
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-08 21:10:08 +0800

    :heavy_plus_sign:  credits topup

---
+++ SFC.Canteen/Views/TopUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace SFC.Canteen.Views
+{
+    /// <summary>
+    /// Interaction logic for TopUp.xaml
+    /// </summary>
+    public partial class TopUp : Window
+    {
+        public TopUp()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void AcceptClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
