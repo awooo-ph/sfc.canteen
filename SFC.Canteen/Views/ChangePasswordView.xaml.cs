456b00f38f194e156c7b7814b6bff98df7044355
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-12 16:00:08 +0800

    :heavy_plus_sign:  change user password

---
+++ SFC.Canteen/Views/ChangePasswordView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SFC.Canteen.Models;
+using SFC.Canteen.ViewModels;
+
+namespace SFC.Canteen.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangePasswordView.xaml
+    /// </summary>
+    public partial class ChangePasswordView : Window
+    {
+        public ChangePasswordView()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (NewPassword.Password != NewPassword2.Password)
+            {
+                MessageBox.Show("Password did not match");
+                return;
+            }
+            if (string.IsNullOrEmpty(NewPassword.Password))
+            {
+                MessageBox.Show("Password is required.");
+                return;
+            }
+            if (MainViewModel.Instance.CurrentUser?.Password != CurrentPassword.Password)
+            {
+                MessageBox.Show("Invalid password.");
+                return;
+            }
+
+            MainViewModel.Instance.CurrentUser.Update(nameof(User.Password), NewPassword.Password);
+            
+            DialogResult = true;
+            Close();
+        }
+    }
+}
