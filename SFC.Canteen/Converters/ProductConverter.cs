915bbe39f96bafbb9ec9b711282868311014fb29
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-27 17:33:02 +0800

    metro ui

---
+++ SFC.Canteen/Converters/ProductConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFC.Canteen.Models;
+
+namespace SFC.Canteen.Converters
+{
+    class ProductConverter : ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            var prod = value as Product;
+            if (prod == null) return "PRODUCT CODE";
+            return prod.ToString();
+        }
+    }
+}
